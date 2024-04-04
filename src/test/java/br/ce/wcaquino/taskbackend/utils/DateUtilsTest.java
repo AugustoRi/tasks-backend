db6c6b4e22fe760313b5f2041d5df254067d0911
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2024-04-04 18:07:26 -0300

    test: apply tests in DateUtils

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,27 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+
+    @Test
+    public void handleReturnFalseToPassedDates() {
+        LocalDate date = LocalDate.of(2010, 1, 1);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void handleReturnTrueToActualDate() {
+        LocalDate date = LocalDate.now();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void handleReturnTrueToFutureDates() {
+        LocalDate date = LocalDate.of(2050, 1, 1);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
