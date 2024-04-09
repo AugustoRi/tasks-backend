3a1fac9151a372cc2e43422fd0baed7861298a92
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2024-04-08 21:59:42 -0300

    test: provocando falha nos testes

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,7 +10,7 @@ public class DateUtilsTest {
     @Test
     public void handleReturnFalseToPassedDates() {
         LocalDate date = LocalDate.of(2010, 1, 1);
-        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
     }
 
     @Test
