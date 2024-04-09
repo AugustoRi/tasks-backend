a43284499e69c4fbf609556e4f1a4af20a50e4a0
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2024-04-08 22:02:06 -0300

    test: fix DateUtilsTest

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,7 +10,7 @@ public class DateUtilsTest {
     @Test
     public void handleReturnFalseToPassedDates() {
         LocalDate date = LocalDate.of(2010, 1, 1);
-        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
     }
 
     @Test
